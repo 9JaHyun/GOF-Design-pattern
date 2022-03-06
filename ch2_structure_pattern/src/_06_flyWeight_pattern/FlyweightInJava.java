df11d5bcb3515c430726e4689ff847d97739b2f1
9JaHyun <jahyunnine@gmail.com>
2022-03-07 00:00:48 +0900

    feat: flyweight pattern

--- ch2_structure_pattern/src/_06_flyWeight_pattern/FlyweightInJava.java
+++ ch2_structure_pattern/src/_06_flyWeight_pattern/FlyweightInJava.java
@@ -3,9 +3,15 @@ package _06_flyWeight_pattern;
 public class FlyweightInJava {
 
     public static void main(String[] args) {
-        // 정적 팩토리 메서드 valueOf() 일정범위의 값들은 캐싱을 사용한다. => Flyweight
         Integer i1 = Integer.valueOf(5);
         Integer i2 = Integer.valueOf(5);
-        System.out.println(i1 == i2); // 캐싱되어 있기 때문에 동일성 '==' 비교가 참이 나온다.
+        System.out.println(i1 == i2);        // True
+        System.out.println(i1.equals(i2));   // True
+
+        // 일정범위를 초가하면 새로운 인스턴스로 생성
+        Integer i3 = Integer.valueOf(200);
+        Integer i4 = Integer.valueOf(200);
+        System.out.println(i3 == i4);        // False
+        System.out.println(i3.equals(i4));   // True
     }
 }
